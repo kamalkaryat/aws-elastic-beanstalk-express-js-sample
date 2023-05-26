08b7701afecb82bb7055fd61fc4b60db176a5897
Kamal Singh Karayat <kamal.karayat@inadev.com>
2023-05-26 18:01:26 +0530

    changes to test ci

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Using AWS Elastic Beanstalk- PaaS'));
+app.get('/', (req, res) => res.send('Using AWS Elastic Beanstalk- PaaS. Changed msg'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
