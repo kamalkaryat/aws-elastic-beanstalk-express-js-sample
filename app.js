f5852c4caa65a9a898009fd2d81141dea257cd33
Kamal Singh Karayat <kamal.karayat@inadev.com>
2023-05-26 15:09:00 +0530

    modified app.js file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Performing some task'));
+app.get('/', (req, res) => res.send('Using AWS Elastic Beanstalk- PaaS'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
